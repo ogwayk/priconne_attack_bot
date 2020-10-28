e8b71bd3d781b6e58261c745777e971c4af3ef84
s1eamed <uncle.sum1961@gmail.com>
2020-10-29 00:14:26 +0900

    ()取り忘れ

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ async def on_message(message):
         # 数字のスタンプのリアクションのメンバーを取得
         text = ''
         member_dict = {}
-        guild_users = guild.members()
+        guild_users = guild.members
         print(guild.member_count)
         print(guild_users)
         if len(reactions) > 0:
