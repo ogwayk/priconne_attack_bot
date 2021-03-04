6733cc42ef10d9b92c3e57b322295f8c6d6c310b
s1eamed <uncle.sum1961@gmail.com>
2021-03-05 03:00:05 +0900

    スプレッドシートに接続テスト

--- bot.py
+++ bot.py
@@ -5,10 +5,20 @@ import sys
 import re
 import datetime
 import os
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
 intents = discord.Intents.all()
 intents.typing = False
 client = discord.Client(intents = intents)
+
+# uma
+credentials = ServiceAccountCredentials.from_json_keyfile_name('../uma-uma-2ae928c61e21.json', scope)
+SPREADSHEET_KEY = '1hL24DKheeJbmvun4UslMN88yDAVqv-iLhKyLJdKPuqw'
 #client = discord.Client()
 
 @client.event
@@ -27,49 +37,56 @@ async def on_message(message):
             await client.logout()
             return
 
-        # 凸完了チャンネルの取得
-        guild = client.get_guild(int(os.environ['GUILD_ID']))
-        channel = get_channel(guild)
-        print(guild.name)
-        # 凸完了チャンネルからメッセージを拾ってくる
-        # print(channel.name)
-        histories = await channel.history(limit = None).flatten()
-        reactions = await get_today_reactions(histories, channel)
-        # print(reactions)
-        # 数字のスタンプのリアクションのメンバーを取得
-        text = ''
-        member_dict = {}
+def get_uma_data(message):
+    gc = gspread.authorize(credentials)
+    worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+    import_value = worksheet.acell('A2').value
+    await reply(message, message.author.mention, import_value)
 
-        channel_users = channel.members
-        print(channel_users)
-        if len(reactions) > 0:
-            for reaction in reactions:
-                users = await reaction.users().flatten()
-                # print(users)
-                match = re.search(r'\d', reaction.emoji)
-                if match is not None:
-                    member_dict[match.group()] = get_reaction_member(users, channel_users)
-        else:
-             text = '私、相手のスリーサイズはわかるんですが今日の残り凸はわかりません…。'
-             await reply(message, message.author.mention, text)
-        
-        # print(member_dict)
-        # n+1凸にスタンプ押している人をn凸のリストから削除する
-        if "1" in member_dict and "2" in member_dict:
-           member_dict['1'] = list(set(member_dict['1']) - set(member_dict['2']))
-        if "2" in member_dict and "3" in member_dict:
-            member_dict['2'] = list(set(member_dict['2']) - set(member_dict['3']))
-        
-        # リプライを作成
-        attack_count = 0
-        for num in [3, 2, 1]:
-            dict_value = []
-            if str(num) in member_dict:
-                dict_value = member_dict[str(num)]
-                attack_count += len(dict_value) * num
-            text += '残り{0}凸の人\n{1}\n\n'.format(num, ', '.join(dict_value), len(dict_value) * num)
-        text += '全体残り凸数：{0}'.format(attack_count)
-        await reply(message, message.author.mention, text)
+def pricone(message):
+    # 凸完了チャンネルの取得
+    guild = client.get_guild(int(os.environ['GUILD_ID']))
+    channel = get_channel(guild)
+    print(guild.name)
+    # 凸完了チャンネルからメッセージを拾ってくる
+    # print(channel.name)
+    histories = await channel.history(limit = None).flatten()
+    reactions = await get_today_reactions(histories, channel)
+    # print(reactions)
+    # 数字のスタンプのリアクションのメンバーを取得
+    text = ''
+    member_dict = {}
+
+    channel_users = channel.members
+    print(channel_users)
+    if len(reactions) > 0:
+        for reaction in reactions:
+            users = await reaction.users().flatten()
+            # print(users)
+            match = re.search(r'\d', reaction.emoji)
+            if match is not None:
+                member_dict[match.group()] = get_reaction_member(users, channel_users)
+    else:
+            text = '私、相手のスリーサイズはわかるんですが今日の残り凸はわかりません…。'
+            await reply(message, message.author.mention, text)
+    
+    # print(member_dict)
+    # n+1凸にスタンプ押している人をn凸のリストから削除する
+    if "1" in member_dict and "2" in member_dict:
+        member_dict['1'] = list(set(member_dict['1']) - set(member_dict['2']))
+    if "2" in member_dict and "3" in member_dict:
+        member_dict['2'] = list(set(member_dict['2']) - set(member_dict['3']))
+    
+    # リプライを作成
+    attack_count = 0
+    for num in [3, 2, 1]:
+        dict_value = []
+        if str(num) in member_dict:
+            dict_value = member_dict[str(num)]
+            attack_count += len(dict_value) * num
+        text += '残り{0}凸の人\n{1}\n\n'.format(num, ', '.join(dict_value), len(dict_value) * num)
+    text += '全体残り凸数：{0}'.format(attack_count)
+    await reply(message, message.author.mention, text)
 
 # 凸完了チャンネルを取得する
 def get_channel(guild):
